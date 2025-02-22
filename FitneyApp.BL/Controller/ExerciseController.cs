7f4b194aa40c35f50e276b3102a5734619a2c771
Ivan Plastinin <plastininbiz@yandex.ru>
2025-02-22 17:28:31 +0300

    Создал exercise controller, exercise и activity

---
+++ FitneyApp.BL/Controller/ExerciseController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FitneyApp.BL.Model;
+
+namespace FitneyApp.BL.Controller
+{
+
+    public class ExerciseController : ControllerBase
+    {
+        private readonly User user;
+
+        private const string EXERCISES_FILE_NAME = "exercises.dat";
+        private const string ACTIVITIES_FILE_NAME = "activities.dat";
+        public List<Exercise> Exercises;
+        public List<Activity> Activities;
+
+        public ExerciseController(User user)
+        {
+            this.user = user ?? throw new ArgumentNullException(nameof(user));
+            Exercises = GetAllExercises();
+            Activities = GetAllActivities();
+        }
+
+        private List<Activity> GetAllActivities()
+        {
+            return Load<List<Activity>>(ACTIVITIES_FILE_NAME) ?? new List<Activity>();
+        }
+
+        private List<Exercise> GetAllExercises()
+        {
+            return Load<List<Exercise>>(EXERCISES_FILE_NAME) ?? new List<Exercise>();
+        }
+
+        public void Add(Activity activity, DateTime begin, DateTime finish)
+        {
+            var act = Activities.SingleOrDefault(a => a.Name == activity.Name);
+
+            if (act == null)
+            {
+                Activities.Add(activity);
+
+                var exercise = new Exercise(begin, finish, activity, user);
+                Exercises.Add(exercise);
+
+            }
+            else
+            {
+                var exercise = new Exercise(begin, finish, act, user);
+                Exercises.Add(exercise);
+
+            }
+            Save();
+        }
+
+        private void Save()
+        {
+            Save(EXERCISES_FILE_NAME, Exercises);
+            Save(ACTIVITIES_FILE_NAME, Activities);
+        }
+    }
+}
---
+++ FitneyApp.BL/Model/Exercise.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FitneyApp.BL.Model
+{
+    [Serializable]
+    public class Exercise
+    {
+        public User User { get; }
+        public DateTime Start { get; }
+        public DateTime End { get; }
+        public Activity Activity { get; }
+
+        public Exercise(DateTime start, DateTime end, Activity activity, User user)
+        {
+            // Проверка
+            Start = start;
+            End = end;
+            Activity = activity;
+            User = user;
+        }
+    }
+}
